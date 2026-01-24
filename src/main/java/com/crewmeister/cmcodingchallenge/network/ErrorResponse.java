223f7bb99df2c98315659ed44d58da25be6fb3c3
Mina Hanna <minasamir1994@gmail.com>
2026-01-25 01:15:11 +0200

    Add generic ErrorResponse

---
+++ src/main/java/com/crewmeister/cmcodingchallenge/network/ErrorResponse.java
@@ -0,0 +1,16 @@
+package com.crewmeister.cmcodingchallenge.network;
+
+import lombok.Builder;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+@Builder
+public class ErrorResponse {
+    private LocalDateTime timestamp;
+    private Integer status;
+    private String error;
+    private String message;
+    private String path;
+}
