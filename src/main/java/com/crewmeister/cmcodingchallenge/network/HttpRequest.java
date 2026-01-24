924e41b7e4c5f60fe788e48a3e4a7a7001c03e2a
Mina Hanna <minasamir1994@gmail.com>
2026-01-24 21:57:51 +0200

    Update HttpRequest headers to MultiValueMap

--- src/main/java/com/crewmeister/cmcodingchallenge/network/HttpRequest.java
+++ src/main/java/com/crewmeister/cmcodingchallenge/network/HttpRequest.java
@@ -2,13 +2,12 @@ package com.crewmeister.cmcodingchallenge.network;
 
 import lombok.Builder;
 import lombok.Getter;
-
-import java.util.Map;
+import org.springframework.util.MultiValueMap;
 
 @Getter
 @Builder
 public class HttpRequest {
-    private final Map<String, String> headers;
+    private final MultiValueMap<String, String> headers;
     private final HttpMethod method;
     private final String url;
     private final Object body;
