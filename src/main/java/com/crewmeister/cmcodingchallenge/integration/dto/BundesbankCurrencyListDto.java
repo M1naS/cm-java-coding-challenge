2d322d59c5101b2ab85fe3af48df22894e7d5dd7
Mina Hanna <minasamir1994@gmail.com>
2026-01-25 23:00:41 +0200

    Implement Bundesbank currency data mapping

---
+++ src/main/java/com/crewmeister/cmcodingchallenge/integration/dto/BundesbankCurrencyListDto.java
@@ -0,0 +1,47 @@
+package com.crewmeister.cmcodingchallenge.integration.dto;
+
+
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.databind.DeserializationContext;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.deser.std.StdDeserializer;
+import exception.SerializationException;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+@JsonDeserialize(using = BundesbankCurrencyListDto.Deserializer.class)
+public class BundesbankCurrencyListDto extends ArrayList<BundesbankCurrencyDto> {
+    public static class Deserializer extends StdDeserializer<BundesbankCurrencyListDto> {
+        public Deserializer() { super(BundesbankCurrencyListDto.class); }
+        @Override
+        public BundesbankCurrencyListDto deserialize(JsonParser jsonParser, DeserializationContext context) {
+            BundesbankCurrencyListDto currencyList = new BundesbankCurrencyListDto();
+            ObjectMapper mapper = (ObjectMapper) jsonParser.getCodec();
+
+            try {
+                JsonNode root = mapper.readTree(jsonParser);
+                JsonNode currencyNodeArray = root.at("/data/codeLists/0/codes");
+
+                if (currencyNodeArray.isArray()) {
+                    for (JsonNode currencyNode : currencyNodeArray) {
+                        if (!(
+                                currencyNode.get("id").textValue().startsWith("_") ||
+                                currencyNode.get("id").textValue().chars().anyMatch(Character::isDigit)
+                        )) {
+                            currencyList.add(
+                                    mapper.treeToValue(currencyNode, BundesbankCurrencyDto.class)
+                            );
+                        }
+                    }
+                }
+            } catch (IOException ioException) {
+                throw new SerializationException("Could not deserialize currency list", ioException);
+            }
+
+            return currencyList;
+        }
+    }
+}
