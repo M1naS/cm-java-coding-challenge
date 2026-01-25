473787b1168dd0a2ca1fac76d7cb19a10bb7f930
Mina Hanna <minasamir1994@gmail.com>
2026-01-25 03:27:24 +0200

    Add BundesbankProperties and URLs in application.properties

---
+++ src/main/java/com/crewmeister/cmcodingchallenge/config/BundesbankProperties.java
@@ -0,0 +1,16 @@
+package com.crewmeister.cmcodingchallenge.config;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+@Getter
+@Setter
+@Configuration
+@ConfigurationProperties(prefix = "external.api.bundesbank")
+public class BundesbankProperties {
+    private String baseUrl;
+    private String specifiedCodelistPath;
+    private String specifiedCodelistFormat;
+}
