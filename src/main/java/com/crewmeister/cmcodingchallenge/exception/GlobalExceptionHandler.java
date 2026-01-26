0b6e2af77b6cb2623f0f738587f38ac4f367fdae
Mina Hanna <minasamir1994@gmail.com>
2026-01-26 04:11:39 +0200

    Add handling of BundesbankExchangeRateException in ExceptionHandler

--- src/main/java/com/crewmeister/cmcodingchallenge/exception/GlobalExceptionHandler.java
+++ src/main/java/com/crewmeister/cmcodingchallenge/exception/GlobalExceptionHandler.java
@@ -1,7 +1,11 @@
 package com.crewmeister.cmcodingchallenge.exception;
 
 import com.crewmeister.cmcodingchallenge.network.ErrorResponse;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -12,6 +16,8 @@ import java.time.LocalDateTime;
 @RestControllerAdvice
 @Slf4j
 public class GlobalExceptionHandler {
+    private final ObjectMapper objectMapper = new ObjectMapper();
+
     @ExceptionHandler(AppException.class)
     public ResponseEntity<ErrorResponse> handleAppException(
             AppException appException,
@@ -30,6 +36,44 @@ public class GlobalExceptionHandler {
         return new ResponseEntity<>(errorResponse, appException.getStatus());
     }
 
+    @ExceptionHandler(BundesbankExchangeRateException.class)
+    public ResponseEntity<ErrorResponse> handleBundesbankExchangeRateException(
+            BundesbankExchangeRateException bundesbankExchangeRateException,
+            HttpServletRequest request
+    ) {
+        ErrorResponse errorResponse;
+        int status;
+
+        try {
+            JsonNode root = objectMapper.readTree(bundesbankExchangeRateException.getMessage());
+
+            status = root.path("status").asInt(500);
+            String message = root.path("title").asText("An unexpected error occurred");
+
+            errorResponse = ErrorResponse.builder()
+                    .timestamp(LocalDateTime.now())
+                    .status(status)
+                    .error(HttpStatus.valueOf(status).getReasonPhrase())
+                    .message(message)
+                    .path(request.getRequestURI())
+                    .build();
+
+            log.error(message);
+
+        } catch (JsonProcessingException jsonProcessingException) {
+            log.error("Could not deserialize error response");
+
+            return handleSerializationException(
+                    new SerializationException(
+                            "Could not deserialize error response",
+                            jsonProcessingException
+                    ),
+                    request
+            );
+        }
+
+        return new ResponseEntity<>(errorResponse, HttpStatus.valueOf(status));
+    }
 
     @ExceptionHandler(SerializationException.class)
     public ResponseEntity<ErrorResponse> handleSerializationException(
