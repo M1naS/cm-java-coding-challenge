79da4244b11df6c2d077d0d8049a1913ce7aa8d3
Mina Hanna <minasamir1994@gmail.com>
2026-01-27 00:27:56 +0200

    Add getExchangeRates request

--- src/main/java/com/crewmeister/cmcodingchallenge/controller/BundesbankExchangeRateController.java
+++ src/main/java/com/crewmeister/cmcodingchallenge/controller/BundesbankExchangeRateController.java
@@ -2,9 +2,7 @@ package com.crewmeister.cmcodingchallenge.controller;
 
 import com.crewmeister.cmcodingchallenge.exception.AppException;
 import com.crewmeister.cmcodingchallenge.integration.ExchangeRateProvider;
-import com.crewmeister.cmcodingchallenge.integration.dto.BundesbankCurrencyRequest;
-import com.crewmeister.cmcodingchallenge.integration.dto.CurrencyDto;
-import com.crewmeister.cmcodingchallenge.integration.dto.CurrencyRequest;
+import com.crewmeister.cmcodingchallenge.integration.dto.*;
 import com.crewmeister.cmcodingchallenge.network.AppResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -41,6 +39,34 @@ public class BundesbankExchangeRateController {
         );
         return new ResponseEntity<>(
                 currenciesAppResponse,
+                HttpStatus.OK
+        );
+    }
+
+    @GetMapping("/exchange-rates")
+    public ResponseEntity<AppResponse<List<? extends ExchangeDto>>> getExchangeRates(
+            @RequestParam(required = false, defaultValue = "de") String lang,
+            @RequestParam(required = false, defaultValue = "1") Integer lastObservations,
+            @RequestParam(required = false, defaultValue = "bundesbank") String provider
+    ) {
+        if (providers.get(provider) == null) {
+            throw new AppException("Provider not found", HttpStatus.NOT_FOUND);
+        }
+
+        ExchangeRequest exchangeRequest = null;
+        if (provider.equals("bundesbank")) {
+            exchangeRequest = new BundesbankExchangeRequest(lang, lastObservations);
+        } else if (provider.equals("local")) {
+            exchangeRequest = new LocalExchangeRequest();
+        }
+
+        AppResponse<List<? extends ExchangeDto>> exchangeAppResponse = new AppResponse<>(
+                providers.get(provider).getExchangeRates(exchangeRequest),
+                HttpStatus.OK.value()
+        );
+
+        return new ResponseEntity<>(
+                exchangeAppResponse,
                 HttpStatus.OK
         );
     }
