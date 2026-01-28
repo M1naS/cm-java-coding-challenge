b50f80a04d155a7a568874297fcc55444685edbf
Mina Hanna <minasamir1994@gmail.com>
2026-01-29 01:09:52 +0200

    Add missing @EnableScheduling

---
+++ src/main/java/com/crewmeister/cmcodingchallenge/config/SchedulingConfig.java
@@ -0,0 +1,9 @@
+package com.crewmeister.cmcodingchallenge.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@EnableScheduling
+@Configuration
+public class SchedulingConfig {
+}
