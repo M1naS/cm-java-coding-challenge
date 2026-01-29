eaf4e49eebc689e32f00d96cb91d387071a78180
Mina Hanna <minasamir1994@gmail.com>
2026-01-29 17:56:53 +0200

    Fix autowiring issue

--- src/main/java/com/crewmeister/cmcodingchallenge/jobs/CacheScheduler.java
+++ src/main/java/com/crewmeister/cmcodingchallenge/jobs/CacheScheduler.java
@@ -10,7 +10,7 @@ import org.springframework.stereotype.Component;
 @Slf4j
 @Component
 public class CacheScheduler {
-    BundesbankCacheManager bundesbankCacheManager;
+    private final BundesbankCacheManager bundesbankCacheManager;
 
     @Scheduled(cron = "0 30 16 * * MON-FRI", zone = "CET")
     public void refreshCache() {
