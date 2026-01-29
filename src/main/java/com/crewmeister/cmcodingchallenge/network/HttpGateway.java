5ff4f21fce35a4a9b5de00718e769fb8cbb120e9
Mina Hanna <minasamir1994@gmail.com>
2026-01-29 19:21:53 +0200

    Add stream method to the HttpGateway

--- src/main/java/com/crewmeister/cmcodingchallenge/network/HttpGateway.java
+++ src/main/java/com/crewmeister/cmcodingchallenge/network/HttpGateway.java
@@ -2,6 +2,9 @@ package com.crewmeister.cmcodingchallenge.network;
 
 import org.springframework.http.ResponseEntity;
 
+import java.io.InputStream;
+
 public interface HttpGateway {
-    <T> ResponseEntity<T> send(AppRequest request, Class<T> responseType);
+    <T> ResponseEntity<T> send(AppRequest appRequest, Class<T> responseType);
+    <T> InputStream stream(AppRequest appRequest, Class<T> requestType);
 }
--- src/main/java/com/crewmeister/cmcodingchallenge/network/impl/RestTemplateGateway.java
+++ src/main/java/com/crewmeister/cmcodingchallenge/network/impl/RestTemplateGateway.java
@@ -3,11 +3,10 @@ package com.crewmeister.cmcodingchallenge.network.impl;
 import com.crewmeister.cmcodingchallenge.network.HttpGateway;
 import com.crewmeister.cmcodingchallenge.network.AppRequest;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.*;
 import org.springframework.web.client.RestTemplate;
+
+import java.io.InputStream;
 
 @RequiredArgsConstructor
 public class RestTemplateGateway implements HttpGateway {
@@ -15,14 +14,26 @@ public class RestTemplateGateway implements HttpGateway {
     private final RestTemplate restTemplate;
 
     @Override
-    public <T> ResponseEntity<T> send(AppRequest request, Class<T> responseType) {
+    public <T> ResponseEntity<T> send(AppRequest appRequest, Class<T> responseType) {
         ResponseEntity<T> responseEntity = restTemplate.exchange(
-                request.getUrl(),
-                HttpMethod.valueOf(request.getMethod().toString()),
-                new HttpEntity<>(request.getBody(), request.getHeaders()),
+                appRequest.getUrl(),
+                HttpMethod.valueOf(appRequest.getMethod().toString()),
+                new HttpEntity<>(appRequest.getBody(), appRequest.getHeaders()),
                 responseType
         );
 
         return new ResponseEntity<>(responseEntity.getBody(), HttpStatus.valueOf(responseEntity.getStatusCodeValue()));
+    }
+
+    @Override
+    public <T> InputStream stream(AppRequest appRequest, Class<T> requestType) {
+        return restTemplate.execute(appRequest.getUrl(),
+                HttpMethod.valueOf(appRequest.getMethod().toString()),
+                restTemplate.httpEntityCallback(
+                        new HttpEntity<>(appRequest.getBody(), appRequest.getHeaders()),
+                        requestType
+                ),
+                HttpInputMessage::getBody
+        );
     }
 }
